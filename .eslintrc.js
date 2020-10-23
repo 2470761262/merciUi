6dd1e6175fe82267f975d5a1cfeec73b27e4d2a0
zr <247076126@qq.com>
2020-10-24 02:51:12 +0800

    添加ts验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
-  root: true,
-  //  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module',
-    parser: 'babel-eslint',
-    allowImportExportEverywhere: true
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
   env: {
+    jest: true,
     browser: true,
     node: true,
     es6: true
   },
+  plugins: ['vue', 'typescript'],
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
     'airbnb-base',
-    '@vue/standard'
+    '@vue/standard',
+    'typescript'
   ],
-  // extends:'airbnb-base',
-  // required to lint *.vue files
-  plugins: ['html', 'vue'],
-  // add your custom rules here
-  // it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'global-require': 0,
@@ -29,6 +24,5 @@ module.exports = {
     'no-console': 'off',
     'no-multiple-empty-lines': [1],
     'comma-dangle': [2, 'never']
-
   }
 }
